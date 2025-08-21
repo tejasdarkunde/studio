c6dafc161506f31fcea59c394579c6d210a41dfd
Aniket Darkunde <aniketdarkunde@gmail.com>
2025-08-21 07:45:30 +0000

    import template & new participant form not updated

--- src/components/features/participants-table.tsx
+++ src/components/features/participants-table.tsx
@@ -41,7 +41,7 @@ export function ParticipantsTable({ participants }: ParticipantsTableProps) {
     }).join('\n');
 
     const csvContent = headers + csvRows;
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-t;' });
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
     const link = document.createElement('a');
 
     const url = URL.createObjectURL(blob);
