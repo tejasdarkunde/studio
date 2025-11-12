dc31d50a654e079446b958b0d16cfc55aec1a34b
Aniket Darkunde <aniketdarkunde@gmail.com>
2025-11-12 07:13:09 +0000

    Make another Home2 page and keep it blank i want to redesign it so keep

---
+++ src/app/home2/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home2Page() {
+  return (
+    <main className="container mx-auto p-4 md:p-8">
+      {/* This page is intentionally left blank for redesign. */}
+    </main>
+  );
+}
